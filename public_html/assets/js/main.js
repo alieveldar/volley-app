50f795748871c3800b8c9cb5be2648fe62e22799
alieveldar <illuminatiday@gmail.com>
2018-11-29 22:32:12 +0300

    #024 Adding Fix to main.js user shed

--- public_html/assets/js/main.js
+++ public_html/assets/js/main.js
@@ -6,16 +6,21 @@ $(document).ready(function() {
     vkUsers.on("click", function(event) {
         var target = $(event.target);
         var id = target.data("vkid");
+        var trid = target.data("trid");
         var sched = target.data("sched");
+        var hideUser = $(".u" + id + trid);
+        
         if (sched == "") {
             target.text("Отписаться");
             target.data("sched", 1);
         } else if (sched == 1) {
             target.text("Записаться");
             target.data("sched" , 2);
+            hideUser.hide();
         } else if (sched == 2) {
             target.text("Отписаться");
             target.data("sched", 1);
+            hideUser.show();
         }
         
         var uri = "/api.php?action=";
--- public_html/templates/colusers.tpl
+++ public_html/templates/colusers.tpl
@@ -1,4 +1,4 @@
-<div class="col-3" data-userssid="{USERSSID}">
+<div class="col-3 u{USERSSID}" data-userssid="{USERSSID}">
     <div class="user"> 
         <div><img src="{AVATAR_URL}" alt="..." class="rounded-circle" style="width: 50px; height: 50px;">
         </div>
